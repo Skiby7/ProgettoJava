597a93aa14ca7f3f59abc3de89c2c752da8b7ebb
Skiby7 <l.scoppitto@studenti.unipi.it>
2020-11-20 13:20:17 +0100

    Label not working

--- src/MicroBlogGUI.java
+++ src/MicroBlogGUI.java
@@ -4,22 +4,28 @@ import java.awt.*;
 public class MicroBlogGUI {
     public static void main(String[] args) {
 
-        JFrame microblog =new JFrame();//creating instance of JFrame
+        ImageIcon image = new ImageIcon("logo.png"); // logo
 
-        JButton b = new JButton("click");//creating instance of JButton
+        JLabel post = new JLabel();
+        post.setText("Posts:");
+        post.setIcon(image);
+        post.setHorizontalTextPosition(JLabel.CENTER);
+        post.setVerticalTextPosition(JLabel.TOP);
+        post.setForeground(Color.white);
+        post.setFont(new Font("Fantasque Mono Sans", Font.PLAIN, 20));
+
+        JFrame microblog = new JFrame();//creating instance of JFrame
         microblog.setVisible(true);//making the frame visible
         microblog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Kills the app when exit
         microblog.setTitle("MicroBlog");
-        microblog.setResizable(true);
-        ImageIcon image = new ImageIcon("logo.png"); // logo
+        microblog.setResizable(false);
         microblog.getContentPane().setBackground(new Color(48, 54, 64));
-        microblog.setIconImage(image.getImage());
-        b.setBounds(130,100,100, 40);
-
-        microblog.add(b);//adding button in JFrame
-
-        microblog.setSize(400,500);//400 width and 500 height
+        microblog.setSize(800,600);//400 width and 500 height
         microblog.setLayout(null);//using no layout managers
+
+        microblog.add(post);
+
+
 
     }
     }
