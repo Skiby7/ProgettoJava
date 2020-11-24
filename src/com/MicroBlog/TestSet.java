85369c694abd6e1d2c1f642f630bc7d97ba6b689
Skiby7 <l.scoppitto@studenti.unipi.it>
2020-11-24 19:42:10 +0100

    koto aiuto 2

--- src/com/MicroBlog/TestSet.java
+++ src/com/MicroBlog/TestSet.java
@@ -1,189 +1,72 @@
-//package com.MicroBlog;
-//
-//import com.MicroBlog.CustomExceptions.*;
-//
-//import java.util.*;
-//
-//public class TestSet {
-//    public static void main(String args[]){
-//        String benvenuto = "Benvenuto su MicroBlog!";
-//        System.out.println(benvenuto);
-//        int post_counter = 0;
-//        SocialNetwork network = new SocialNetwork();
-//        String[] users = {"Roberto", "Leonardo", "Fernando", "Giacomo","Lorenzo", "Giulia","Luisa","Elena","Erica", "Francesca"};
-//        List<Post> postList = new LinkedList<>();
-//        List<String> words = new LinkedList<>();
-//        words.add("force");
-//        words.add("back");
-//        words.add("conf");
-//        words.add("Leonardo");
-//        words.add("It's");
-//
-//
-//        for (String user: users){
-//            for (int i = 0; i < 5; i++){
-//                try {
-//                    String toAdd = "Ciao, sono " + user + " e questo e' il post numero " + i;
-//                    Post newPost = new Post(user, toAdd, ++post_counter);
-//                    network.addPost(newPost);
-//                    if (i > 1 && i < 4 && (user.equals("Luisa") || user.equals("Edoardo") || user.equals("Leonardo")))
-//                        postList.add(newPost);
-//                }catch (IllegalLengthException | EmptyTextException error){
-//                    System.out.println(error);
-//                }
-//            }
-//        }
-//
-//        try {
-//            Post newPost = new Post("Rocky", "Adrianaaa!!!", ++post_counter);
-//            network.addPost(newPost);
-//        }catch (IllegalLengthException | EmptyTextException error){
-//            System.out.println(error);
-//        }
-//
-//
-//        try {
-//            Post newPost = new Post("Mario", "It's me, Mario!", ++post_counter);
-//            network.addPost(newPost);
-//        }catch (IllegalLengthException | EmptyTextException error){
-//            System.out.println(error);
-//        }
-//
-//        try {
-//            Post newPost = new Post("Pacman", "echo \"ILoveCandy\" >> /etc/pacman.conf", ++post_counter);
-//            network.addPost(newPost);
-//            postList.add(newPost);
-//        }catch (IllegalLengthException | EmptyTextException error){
-//            System.out.println(error);
-//        }
-//        for (String user: users)
-//            try {
-//                network.follow(post_counter, user);
-//            }catch (AutoFollowException error){
-//                System.out.println(error);
-//            }
-//
-//
-//
-//        try {
-//            Post newPost = new Post("Yoda", "May the force be with you", ++post_counter);
-//            network.addPost(newPost);
-//            postList.add(newPost);
-//        }catch (IllegalLengthException | EmptyTextException error){
-//            System.out.println(error);
-//        }
-//
-//        try {
-//            Post newPost = new Post("Ratchet & Clank", "We're back!", ++post_counter);
-//            network.addPost(newPost);
-//        }catch (IllegalLengthException | EmptyTextException error){
-//            System.out.println(error);
-//        }
-//
-//        System.out.println("Creo una rete sociale contenente gli utenti: " + Arrays.toString(users));
-//        System.out.println("Ogni utente fra questi posta la frase \"Ciao, sono 'user' e questo e' il post numero 'i' per 5 volte");
-//        System.out.println("Aggiungo poi altri 5 utenti(Yoda, Pacman, Ratchet & Clank, Rocky e Mario)\n i quali creano un post ciascuno e interagiscono con gli altri utenti.");
-//
-//
-//        for (int i = 1; i < 51; i++){
-//            if(i >= 6 && i <= 10)
-//                try {
-//                    network.follow(i, "Roberto");
-//                    network.follow(i, "Fernando");
-//                    network.follow(i, "Giacomo");
-//                    network.follow(i, "Elena");
-//                    network.follow(i, "Luisa");
-//                    network.follow(i,"Pacman");
-//                    network.follow(i,"Ratchet & Clank");
-//                    network.follow(i,"Yoda");
-//                }catch (AutoFollowException Error){
-//                    System.out.println(Error);
-//                }
-//            if (i>=36 && i<= 40)
-//                try {
-//                    network.follow(i, "Roberto");
-//                    network.follow(i, "Fernando");
-//                    network.follow(i, "Giacomo");
-//                    network.follow(i, "Luisa");
-//                    network.follow(i,"Rocky");
-//                }catch (AutoFollowException Error){
-//                    System.out.println(Error);
-//                }
-//            if (i>=11 && i<= 15)
-//                try {
-//                    network.follow(i, "Roberto");
-//                    network.follow(i, "Giacomo");
-//                    network.follow(i, "Luisa");
-//                }catch (AutoFollowException Error){
-//                    System.out.println(Error);
-//                }
-//            if (i<= 5)
-//                try {
-//                    network.follow(i, "Giacomo");
-//                    network.follow(i, "Luisa");
-//                }catch (AutoFollowException Error){
-//                    System.out.println(Error);
-//                }
-//
-//        }
-//        for (String user: users)
-//            try {
-//                network.follow(53, user);
-//            }catch (AutoFollowException error){
-//                System.out.println(error);
-//            }
-//
-//        System.out.println("Adesso alcuni utenti seguono i post di altri");
-//        System.out.println("\nGli influencers sono:");
-//        for (String toPrint: network.influencers(network.guessFollowers(postList))){
-//            System.out.println(" - " + toPrint);
-//
-//        }
-//
-//        System.out.println("\n\n");
-//        System.out.println("\nGli utenti iscritti sono:");
-//        for (String toPrint: network.getMentionedUser())
-//            System.out.println(" - " + toPrint);
-//        System.out.println("\n\n");
-//        System.out.println("\nGli utenti che hanno postato nella lista postList sono:");
-//        for (String toPrint: network.getMentionedUser(postList))
-//            System.out.println(" - " + toPrint);
-//        System.out.println("\n\n");
-//        System.out.println("\nI post scritti da Yoda sono:");
-//        for (Post postToPrint : network.writtenBy("Yoda"))
-//            postToPrint.printPost();
-//        System.out.println("\n\n");
-//        System.out.println("I post scritti da Luisa ed Leonardo dentro postList sono:");
-//        for (Post postToPrint: network.writtenBy(postList,"Luisa")) {
-//            System.out.println();
-//            postToPrint.printPost();
-//        }
-//        for (Post postToPrint: network.writtenBy(postList, "Leonardo")) {
-//            System.out.println();
-//            postToPrint.printPost();
-//        }
-//        System.out.println("\n\n");
-//        System.out.println("I post che contengono le parole " + words + " sono:");
-//        for (Post postToPrint: network.containing(words)) {
-//            System.out.println();
-//            postToPrint.printPost();
-//        }
-//        String prova = null;
-//        try {
-//            network.addPost(null);
-//            System.out.println("qui");
-//        }catch (IllegalLengthException e){
-//
-//            System.out.println(e);
-//        }catch (EmptyTextException e) {
-//            e.printStackTrace();
-//        }
-//        Post provanull = null;
-//        try{
-//            network.addPost(provanull);
-//        }catch (IllegalLengthException | EmptyTextException e){
-//            System.out.println(e);
-//        }
-//    }
-//
-//}
+package com.MicroBlog;
+
+import com.MicroBlog.CustomExceptions.*;
+import com.MicroBlog.*;
+
+
+
+public class TestSet {
+    public static void main(String[] args){
+        FamilyFriendlySocialNetwork MicroBlog = new FamilyFriendlySocialNetwork();
+        String tooLong = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id justo in ante consequat consectetur. Vestibulum venenatis rhoncus consequat. Nulla ante nisl, scelerisque ac commodo ut, efficitur id lorem. Cras varius fermentum ipsum, at pretium orci pharetra eget. Vestibulum et leo quis tortor facilisis consequat. Pellentesque aliquam ac elit ut posuere. Duis facilisis lectus sit amet mi hendrerit pharetra. Maecenas eget consequat metus. Integer feugiat fringilla auctor. Maecenas eleifend rutrum tristique.\n" +
+                "\n" +
+                "Maecenas vel mattis ex. Integer tincidunt iaculis ex, nec viverra enim maximus a. Fusce eu placerat justo, ac auctor risus. Donec sem nunc, posuere eu nisl nec, suscipit elementum risus. Morbi id quam at lacus finibus bibendum ac pellentesque arcu. Aenean venenatis egestas tortor in sollicitudin. Fusce eget euismod metus. Donec venenatis maximus orci ac tincidunt. Proin molestie sem orci, sed suscipit augue pulvinar ut. Etiam velit augue, eleifend et. ";
+
+        String[] usernames = {"Alessia", "Matilda", "Carmela", "Rebecca", "Enrico", "Raffaela", "Vinicia", "Carmelo", "Rossana", "Erik", "Alice", "Lorella", "Simone", "Anna", "Fabiano", "Katia", "Arianna", "Oksana", "Angela", "Aicha"};
+
+        String[] post = {"\"Computer viruses are an urban legend.\"\n- Peter Norton, 1988", "\"Le prix Nobel, c’est une bouée de sauvetage lancée à un nageur  qui a déjà atteint la rive.\"\n- George Bernard Shaw", "\"I don’t know what the language of the year 2000 will look like, but I know it will be called Fortran.\"\n– CA Hoare, 1982", "\"Any fool can use a computer.  Many do.\"\n- Ted Nelson", "\"Men are from Mars. Women are from Venus. Computers are from hell.\"", "\"Where is the ‘any’ key?\" \n– Homer Simpson, in response to the message, \"Press any key\"", "\"Looking at code you wrote more than two weeks ago is like looking at code you are seeing for the first time.\"\n– Dan Hurvitz", "\"I don’t care if it works on your machine!  We are not shipping your machine!\"\n- Vidiu Platon", "\"If people never did silly things, nothing intelligent would ever get done.\" \n– Ludwig Wittgenstein", "The world we have created is a product of our thinking; it cannot be changed without changing our thinking.\"\n- Albert Einstein", "\"I have not failed. I've just found 10,000 ways that won't work.\"\n- Thomas A. Edison ", "\"Most of you are familiar with the virtues of a programmer.  There are three, of course: laziness, impatience, and hubris.\"\n- Larry Wall", "\"It does not happen all at once. There is no instant pudding.\"\n- W. Edwards Deming", "Your lean process should be a lean process.\"\n- Author Unknown", "\"In C++ it’s harder to shoot yourself in the foot, but when you do, you blow off your whole leg.\"\n- Bjarne Stroustrup", "He who rejects change is the architect of decay.  The only human institution which rejects progress is the cemetery.\"\n- Harold Wilson", "\"If you think your users are idiots, only idiots will use it.\"\n– Linus Torvalds", "\"If you think your users are idiots, only idiots will use it.\"\n– Linus Torvalds", "\"The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.\"\n- Seymour Cray", "\"In the future, computers may weigh no more than 1.5 tonnes.\"\n– Popular mechanics, 1949"};
+
+        String[] post2 = {"\"There is nothing so useless as doing efficiently that which should not be done at all.\"\n- Peter F. Drucker", "\"From a programmer’s point of view, the user is a peripheral that types when you issue a read request.\"\n- P. Williams", "\"Software is a gas; it expands to fill its container.\"\n- Nathan Myhrvold", "\"I have not failed. I've just found 10,000 ways that won't work.\"\n- Thomas A. Edison ", "\"Life is too short to run proprietary software.\"\n- Bdale Garbee", "\"The central enemy of reliability is complexity.\"\n- Geer et al.", "\"The best way to predict the future is to implement it.\"\n- David Heinemeier Hansson", "\"The more you know, the more you realize you know nothing.\"\n- Socrates", "\"The question of whether computers can think is like the question of whether submarines can swim.\"\n- Edsger W. Dijkstra", "\"I think it’s a new feature.  Don’t tell anyone it was an accident.\"\n- Larry Wall", "\"On n'innove jamais seul dans son coin\"\n- Areva Innovation Motto", "\"Good specifications will always improve programmer productivity far better than any programming tool or technique.\"\n- Milt Bryce", "\"Optimism is an occupational hazard of programming; feedback is the treatment.\"\n- Kent Beck", "\"Looking at code you wrote more than two weeks ago is like looking at code you are seeing for the first time.\"\n- Dan Hurvitz", "\"Programming is like sex: one mistake and you’re providing support for a lifetime.\"\n- Michael Sinz", "\"The result of long-term relationships is better and better quality, and lower and lower costs.\"\n- W. Edwards Deming", "\"Saying that Java is nice because it works on all OSes is like saying that anal sex is nice because it works on all genders.\"\n- Alanna", "\"The proper use of comments is to compensate for our failure to express ourself in code.\"\n- Uncle Bob Martin", "\"Good programmers use their brains, but good guidelines save us having to think out every case.\"\n- Francis Glassborow", "\"Dogs have Owners, Cats have Staff.\""};
+        // Test funzioni base
+        try {
+            MicroBlog.addPost("Leonardo", "Ciao a tutti, mi sono appena iscritto!");
+            MicroBlog.addPost("Pacman", "echo \"ILoveCandy\" >> /etc/pacman.conf");
+            MicroBlog.addPost("Rick", "Hey Morty, come here!");
+            MicroBlog.addPost("Elena", "Sembra interessante questo Social Network");
+            MicroBlog.addPost("null", "");
+            MicroBlog.addPost(null, "null");
+            MicroBlog.addPost("TOO LONG", "");
+        } catch (IllegalLengthException | EmptyTextException error){
+            System.out.println(error);
+        }
+
+        // Popolo il social network
+        int i = 0;
+        for (String user: usernames){
+            try {
+                if(i == 20)
+                    break;
+                MicroBlog.addPost(user, post2[i]);
+                i++;
+            } catch (IllegalLengthException | EmptyTextException e) {
+                System.out.println(e);
+            }
+        }
+
+
+        try {
+            MicroBlog.addPost(null, "null");
+
+        }catch (NullPointerException NULL){
+            System.out.println("Entry non valida!");
+        } catch (IllegalLengthException | EmptyTextException e){
+            System.out.println(e);
+        }
+        try {
+            MicroBlog.addPost("TOO LONG", tooLong);
+        }catch (EmptyTextException | IllegalLengthException e){
+            System.out.println(e);
+        }
+        try {
+            MicroBlog.follow(1, "Elena");
+        }catch (AutoFollowException e){
+            System.out.println("Non ci si può seguire da soli!");
+        }
+        // MicroBlog.printAllPosts();
+
+
+
+
+    }
+
+}
